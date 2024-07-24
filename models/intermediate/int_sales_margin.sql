a38f9d8ac7aa9e0426bb6efdc5f0cd36023e70e0
bravocastanedajose@gmail.com <bravocastanedajose@gmail.com>
2024-07-24 08:28:42 +0000

    add purchase cost and margin

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,11 @@
+SELECT 
+    products_id, 
+    date_date, 
+    orders_id,
+    revenue, 
+    quantity, 
+    ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
+    s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
+FROM {{ ref('stg_data__sales') }} s
+left JOIN {{ ref('stg_data__product') }} p
+  using (products_id)
